63c4c077551081e6bbe0a6bc6fb2ce5ce8aa1cb6
GamingLizard9 <dawsoncall888@gmail.com>
2022-05-13 15:31:09 -0400

    Update test.js

--- test.js
+++ test.js
@@ -20,4 +20,47 @@ function transcribe(dna) {
     }
     return(rna);
 }
-console.log(transcribe("tacgtcagtatt"))
+function translate(rna) {
+    var output = ""
+    if(rna.length % 3 != 0) {
+        return("Error: Wrong length")
+    }
+    for(let i = 0; i < rna.length; i += 3) {
+        //There is a better way to do this and it will be replaced someday
+        switch(rna[i]) {
+            case "a":
+                switch(rna[i+1]) {
+                    case "u":
+                        switch(rna[i+2]) {
+                            case "g":
+                                output += "Met";
+                                break;
+                            default:
+                                output += "Ile";
+                                break;
+                        }
+                        break;
+                    case "c":
+                        output += "Thr";
+                        break;
+                    case "a":
+                        switch(rna[i+2]) {
+                            case "c":
+                            case "u":
+                                output += "Asn";
+                                break;
+                            case "a":
+                            case "g":
+                                output += "Lys";
+                                break;
+                        }
+                        break;
+                }
+        }
+        if(i != rna.length - 3) {
+            output += "|";
+        }
+    }
+    return(output);
+}
+console.log(translate(transcribe("taatactagtattcatcctcgtgttgatgctggtgtttattcttgttt")))
