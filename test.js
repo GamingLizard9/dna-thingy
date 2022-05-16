9627c0d0c3c287e7ec4ef2356e8849a2e9493327
GamingLizard9 <dawsoncall888@gmail.com>
2022-05-15 21:02:55 -0400

    Finished translate function

--- test.js
+++ test.js
@@ -1,3 +1,16 @@
+//All values for converting RNA codons into amino acids (why are there so many)
+const codonTable = {
+    "aaa": "Lys","aac": "Asn","aag": "Lys","aau": "Asn","aca": "Thr","acc": "Thr","acg": "Thr","acu": "Thr",
+    "aga": "Arg","agc": "Ser","agg": "Arg","agu": "Ser","aua": "Ile","auc": "Ile","aug": "Met","auu": "Ile",
+    "caa": "Gln","cac": "His","cag": "Gln","cau": "His","cca": "Pro","ccc": "Pro","ccg": "Pro","ccu": "Pro",
+    "cga": "Arg","cgc": "Arg","cgg": "Arg","cgu": "Arg","cua": "Leu","cuc": "Leu","cug": "Leu","cuu": "Leu",
+    "gaa": "Glu","gac": "Gsp","gag": "Glu","gau": "Gsp","gca": "Ala","gcc": "Ala","gcg": "Ala","gct": "Ala",
+    "gga": "Gly","ggc": "Gly","ggg": "Gly","ggu": "Gly","gua": "Val","guc": "Val","gug": "Val","guu": "Val",
+    "uaa": "Stop","uac":"Tyr","uag": "Stop","uau":"Tyr","uca": "Ser","ucc": "Ser","ucg": "Ser","ucu": "Ser",
+    "uga": "Stop","ugc":"Cys","ugg": "Trp","ugu": "Cys","uua": "Leu","uuc": "Phe","uug": "Leu","uuu": "Phe"
+}
+
+//Function that converts DNA strings to RNA
 function transcribe(dna) {
     var rna = "";
     var dna = dna.toLowerCase()
@@ -20,47 +33,26 @@ function transcribe(dna) {
     }
     return(rna);
 }
+
+//Function that converts RNA strings into amino acids
 function translate(rna) {
     var output = ""
     if(rna.length % 3 != 0) {
         return("Error: Wrong length")
     }
     for(let i = 0; i < rna.length; i += 3) {
-        //There is a better way to do this and it will be replaced someday
-        switch(rna[i]) {
-            case "a":
-                switch(rna[i+1]) {
-                    case "u":
-                        switch(rna[i+2]) {
-                            case "g":
-                                output += "Met";
-                                break;
-                            default:
-                                output += "Ile";
-                                break;
-                        }
-                        break;
-                    case "c":
-                        output += "Thr";
-                        break;
-                    case "a":
-                        switch(rna[i+2]) {
-                            case "c":
-                            case "u":
-                                output += "Asn";
-                                break;
-                            case "a":
-                            case "g":
-                                output += "Lys";
-                                break;
-                        }
-                        break;
-                }
+        var currentCodon = ""
+        for(let j = 0; j < 3; j += 1) {
+            currentCodon += rna.charAt(i+j)
         }
+        output += codonTable[currentCodon];
+        
         if(i != rna.length - 3) {
             output += "|";
         }
     }
     return(output);
 }
-console.log(translate(transcribe("taatactagtattcatcctcgtgttgatgctggtgtttattcttgttt")))
+
+//Run the translation function with a test value
+console.log(translate("aaacccggguuu"))
